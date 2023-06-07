7547260990d4c4955b53dde01adc6994fe5d4fa7
shabeeb-sh <shabeebshaji892@gmail.com>
2023-06-07 15:17:34 +0530

    ems server complete

---
+++ Routes/router.js
@@ -0,0 +1,27 @@
+//import express
+const express = require('express')
+//create router for express
+const router = new express.Router()
+//import controller
+const userController = require('../Controller/userController')
+//import multer upload configuration
+const upload = require('../multerConfig/storageConfig')
+
+
+//define routes for each http request
+router.post('/employee/register',upload.single('user_profile'),userController.register)
+
+//define route for get all users
+router.get('/employee/get-all-employee-details',userController.getusers)
+
+//define route to view profile
+router.get('/employee/view-profile/:id', userController.viewprofile)
+
+//define a route to remove user
+router.delete('/employee/delete-user/:id',userController.deleteUser)
+
+//define route to update user
+router.put('/employee/update/:id',upload.single('user_profile'),userController.editUser)
+
+//export router
+module.exports = router
